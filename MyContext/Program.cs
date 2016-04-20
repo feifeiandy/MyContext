fba2e981dcbca799994aab9e406ddb7fbd3485dc
feifeiandy <feifeiandy@outlook.com>
2016-04-20 11:37:57 +0800

    x

--- MyContext/Program.cs
+++ MyContext/Program.cs
@@ -1,6 +1,7 @@
 ﻿using MyContext.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,19 +14,29 @@ namespace MyContext
         {
 
             aisino_mesContext MyContext = new aisino_mesContext();
+           
+            SysRole sys = new SysRole();
+            sys.RoleCode = "121";
+            sys.Name = "wdwd";
+            sys.Remark = "121";
+            sys.Stopped = false;
+
+            //数据实体在内存中的5种状态
+            //Console.WriteLine(MyContext.Entry(sys).State);
+            //MyContext.SysRoles.Attach(sys);
+            //Console.WriteLine(MyContext.Entry(sys).State);
+            //MyContext.Entry(sys).State = EntityState.Modified;
+            //Console.WriteLine(MyContext.Entry(sys).State);
+            //MyContext.SaveChanges();
+            //Console.WriteLine(MyContext.Entry(sys).State);
+            //Console.Read();
+
+
             //#region ToList() Local 的区别
             ////ToList每次查询数据库，不管内存中是否添加
             ////Local每次查询内存（后来添加的，和数据库本来有的，如果内存中标记删除，数据库中有则被过滤）
             //int listcont = MyContext.SysRoles.ToList().Count();
             //Console.WriteLine("添加实体之前" + listcont);
-
-            //SysRole sys = new SysRole();
-            //sys.RoleCode = "121";
-            //sys.Name = "121";
-            //sys.Remark = "121";
-            //sys.Stopped = false;
-
-            //MyContext.SysRoles.Add(sys);
             //int listcont2 = MyContext.SysRoles.ToList().Count();
             //int listcont3 = MyContext.SysRoles.Local.Count();
             //Console.WriteLine("添加实体之前" + listcont2);
@@ -34,14 +45,49 @@ namespace MyContext
             //#endregion
 
 
-            //#region 集合中添加实体（外键相关），默认相关外键的表自动添加实体
+            #region 集合中添加实体（外键相关），默认相关外键的表自动添加实体
+            //添加的实体有两种情况一个是已经存在，一个是不存在
+            //默认的外键实体是added状态
             //SysRoleUser sysRoleUser = new SysRoleUser();
             //sysRoleUser.RoleCode = "121";
             //sysRoleUser.UserCode = "11";
             //sysRoleUser.SysRole = sys;
+            //Console.WriteLine(MyContext.Entry(sys).State); 
             //MyContext.SysRoleUsers.Add(sysRoleUser);
+            //MyContext.Entry(sys).State = EntityState.Unchanged;
+            //Console.WriteLine(MyContext.Entry(sysRoleUser).State);
+            //Console.WriteLine(MyContext.Entry(sys).State);
+            //Console.ReadLine();
             //MyContext.SaveChanges();
-            //#endregion
+
+
+
+            //?
+            SysRight srt = MyContext.SysRights.Find("ddd");
+            SysRole sr = MyContext.SysRoles.Find("121");
+            MyContext.Entry(sr).Collection("SysRights").Load();
+            foreach (var item in sr.SysRights)
+            {
+                Console.WriteLine(MyContext.Entry(item).State);
+            }
+            Console.WriteLine(MyContext.Entry(sr).State);
+            MyContext.Entry(sr).State = EntityState.Modified;
+            sr.SysRights.Add(srt);
+            Console.WriteLine("-----------------------------");
+            foreach (var item in sr.SysRights)
+            {
+                Console.WriteLine(MyContext.Entry(item).State);
+            }
+            Console.WriteLine(MyContext.Entry(sr).State);
+            Console.WriteLine("*****************************");
+            Console.WriteLine(MyContext.Entry(sr).State);
+            MyContext.SaveChanges();
+            foreach (var item in sr.SysRights)
+            {
+                Console.WriteLine(MyContext.Entry(item).State);
+            }
+            Console.ReadLine();
+            #endregion
 
 
             //#region 级联删除 一对一
